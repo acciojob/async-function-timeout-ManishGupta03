a20e9a7cb9ab1a67dc351cc5ddfbd36c3795e7f1
Manish Gupta <117648576+ManishGupta03@users.noreply.github.com>
2023-11-03 23:12:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+ async function delayAndResolve(delayTime) {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve('Message after delay');
+          }, delayTime);
+        });
+      }
+
+      async function displayMessage() {
+        const outputElement = document.getElementById('output');
+
+        try {
+          const message = await delayAndResolve(3000); // Wait for 3 seconds
+          outputElement.textContent = message;
+        } catch (error) {
+          console.error('An error occurred:', error);
+        }
+      }
+
+      displayMessage();
